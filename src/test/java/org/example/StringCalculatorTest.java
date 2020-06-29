6303e7859a52f03b8bbf548d10fef09ad9bd0609
Chirag Bhatia <chiragbhatia94@gmail.com>
2020-06-30 01:29:33 +0530

    test(stringcalculator): added test to handle multiple custom delimiters

--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -75,5 +75,16 @@ class StringCalculatorTest {
                             "should also handle default delimiters")
             );
         }
+
+        @Test
+        @Disabled
+        void handleCustomDelimiters() {
+            assertAll(
+                    "should handle custom delimiters",
+                    () -> assertEquals(5 + 8 + 7 + 8, stringCalculator.add("//[+][ ]\n5+8+7 8")),
+                    () -> assertEquals(5 + 8 + 7, stringCalculator.add("//[;][****]\n5;8;7****98"),
+                            "should handle delimiter with length > 1 char")
+            );
+        }
     }
 }
